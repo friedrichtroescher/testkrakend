2ebee305f2d89f2d6fdb8176723bdd442fa7d623
Friedrich Troescher <friedrich.troescher@maibornwolff.de>
2024-07-02 16:11:19 +0200

    circuit breaker issues

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
-import time
+from flask import Flask, request, abort
 
 app = Flask(__name__)
+import time
 
-@app.route('/')
+
+@app.route('/api')
 def hello_world():
-    time.sleep(1)
-    return 'Hello World!'
+    # read query parameter "crash"
+    crash = request.args.get('crash')
+    if crash == "false":
+        # raise exception to crash the application
+        return 'Hello World!'
+    status = int(request.args.get('status'))
+    abort(status)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081)
